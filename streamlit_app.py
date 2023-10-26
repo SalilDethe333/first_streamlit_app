51cc15031c8332820f0cdc5b467b664ea6d39886
SalilDethe333 <149050268+SalilDethe333@users.noreply.github.com>
2023-10-27 02:42:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.write('The user entered ', fruit_choice)
 
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/+ fruit_choice")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 
 
 # take json version of the response and normalize it
